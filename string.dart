7930791e86ae17827b0c42386721a3dd5682c2a3
RenkiGunawan <162980320+RenkiGunawan@users.noreply.github.com>
2024-03-11 17:18:06 +0700

    Add files via upload

---
+++ string.dart
@@ -0,0 +1,35 @@
+void main() {
+  String nama = ' Rifqi Eka Hardianto ';
+  String daftarHewan = "Kucing, Kuda, Kambing";
+  var angka = 17;
+
+  // cek apakah mengandung string tertentu
+  print(nama.contains('eka'));
+
+  // mengubah menjadi huruf kecil
+  print(nama.toLowerCase());
+
+  // mengubah menjadi huruf besar
+  print(nama.toUpperCase());
+
+  // mengubah menjadi string
+  print(angka.toString());
+
+  // mengubah menjadi list
+  print(daftarHewan.split(', ')[1]);
+
+  // menampilkan substring
+  print(nama.substring(6, 9));
+  // 6 -> mulai (masuk)
+  // 9 -> akhir (tidak masuk)
+
+  // menampilkan panjang string
+  print(nama.length);
+
+  // menghilangkan spasi di depan dan di belakang
+  print(nama.trim());
+
+  // menghilangkan spasi di depan
+  print(nama.trimRight());
+
+  // menghilangkan…
